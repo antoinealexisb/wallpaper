c9c613fe7028120e0f837f687ecefd234d5a5655
Antoine-Alexis Bourdon <antoine-alexis_bourdon@ens.univ-artois.fr>
2020-04-03 17:21:49 +0200

    README and tool

---
+++ README.md
@@ -0,0 +1,21 @@
+# WallPaper change (Gnome and Xfce)
+
+Hi, I am a tool that works with bash.  
+I can change the wallpaper of the current pc 😁.  
+If you want to prank someone you can thanks to me. (provided that the target computer is on gnome or xfce in graphical interface)  
+
+## Runing Locally
+
+just git clone me and give me the performance rights 😊.  
+```bash
+$ git clone https://github.com/antoinealexisb/wallpaper.git
+$ cd wallpaper
+$ chmod +x farce.sh
+
+$ ./farce.sh
+```
+
+I also have a slightly less soft version, just put an argument (any argument 😂😂).  
+example:
+`$ ./farce.sh hello`
+`$ ./farce.sh coucou`
---
+++ farce.sh
@@ -0,0 +1,79 @@
+#!/bin/bash
+
+###########################################
+#
+# changes the background image on Gnome and Xfce
+#
+# Author : Antoine-Alexis Bourdon
+# Link : www.github.com/...
+# Version : 1.0.0
+# Dependency : libnotify-bin
+#
+###########################################
+
+test "$#" -gt 1 && echo "Number of arguments too high , PLEASE GUY !!!!!!!!!" && exit 1
+
+#if not param is soft and with param it's less soft but not hard.
+if [[ "$#" -eq 0 ]]
+then
+	LINK="https://wallhaven.cc/search?categories=010&purity=100&sorting=random"
+	echo $LINK
+else
+	LINK="https://wallhaven.cc/search?categories=010&purity=010&sorting=random"
+fi
+
+#variables
+fileprefix=$HOME/Images/wallbase-wp-
+todaywp=$fileprefix$(date +%y-%m-%d-%H-%M-%S).jpg
+
+#######################################################
+# Funtion check your internet connection
+# #Args: None
+# #Return: 1 (is good), 0 (no internet connection 😭)
+#######################################################
+function chkinternet {
+	echo `wget -q -O - google.com | grep -c "<title>Google</title>"`
+}
+
+#######################################################
+# note : install libnotify-bin to popup notification
+# Function that sends a pop-up
+# #Args: None
+# #Return : None
+#######################################################
+function notify {
+	if [[ -e /usr/bin/notify-send ]]
+	then
+		notify-send "Hey $USER" "I'm here for you 😊(in your Desktop 😜)" -i $todaywp
+	fi
+}
+
+#Check connection 
+while [ $(chkinternet) != "1" ]; do sleep 15; done
+echo Internet connection is OK
+
+#Donwload IMG
+wget -O - $(wget -O - $LINK | grep -Eo "(https://wallhaven.cc/w/[0-9a-zA-Z]+)" | head -n 1) | grep -Eo "https://w.wallhaven.cc/full/[0-9a-zA-Z]+/wallhaven-[0-9a-zA-Z]+.[a-z]+" | wget -q -i - -O $todaywp 
+
+#Is GOOD Information 😂
+echo Photo downloaded
+
+#According to the different graphics system 
+if [[ -e /usr/bin/gsettings ]]
+then
+	gsettings set org.gnome.desktop.background picture-uri file:///$todaywp
+elif [[ -e /usr/bin/xfconf-query ]]
+then
+	#Not work for ubuntu 18.04.4LTS
+	xfconf-query -c xfce4-desktop -p /backdrop/screen0/monitor0/image-path -s $todaywp
+	xfconf-query -c xfce4-desktop -p /backdrop/screen0/monitor0/workspace0/last-image -s $todaywp
+elif [[ -e /usr/bin/gconftool-2 ]]
+then
+	gconftool-2 --type string --set /desktop/gnome/background/picture_filename $todaywp
+	gconftool-2 --type string --set /desktop/gnome/background/picture_options scaled
+fi
+
+#Notify 😊😊😊😊
+notify
+
+#Thanks for watching me. 😊😊
